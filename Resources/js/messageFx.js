91861ab43f1e776a9e1d1e5a699e220c204920c7
Torrey Rice <rice@itorrey.com>
2013-12-29 19:23:10 -0800

    Updating incoming style to scale up image
    Signed-off-by: Torrey Rice <rice@itorrey.com>

--- Resources/js/messageFx.js
+++ Resources/js/messageFx.js
@@ -16,20 +16,29 @@ function initStyle() {
 			var avi = node.getElementsByClassName("avatar")[0];
 			var mess = node.getElementsByClassName("messages")[0];
 			
-			mess.style.marginLeft = "50px";
-			avi.style.left = "5px";
-			avi.style.opacity = "0";
+			mess.style.marginLeft = "40px";
+			mess.style.opacity = "0.5";
+			avi.style.left = "0";
+			avi.style.opacity = "0.5";
+			
 			
 			move(mess)
 				.ease('out')
-				.add('margin-left', 10)
+				.add('margin-left', 20)
 				.set('opacity', 1)
 				.end();
 				
 			move(avi)
+				.duration('0s')
+				.scale('0.5')
+				.end();
+					
+			move(avi)
+				.duration('0.4s')
 				.ease('out')
-				.add('left', 5)
+				.add('left', 10)
 				.set('opacity', 1)
+				.scale('1')
 				.end();
 		}
 		alignChat(coalescedHTML.shouldScroll);
