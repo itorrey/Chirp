932aecdd06527b76da47d03b4adff82a34c95460
Torrey Rice <rice@sitepen.com>
2013-12-30 23:12:05 -0800

    oops removing commented out code
    Signed-off-by: Torrey Rice <rice@sitepen.com>

--- Resources/js/messageFx.js
+++ Resources/js/messageFx.js
@@ -13,32 +13,6 @@ function initStyle() {
 		
 		if(!coalescedHTML.isConsecutive) {
 			var node = document.getElementById("Chat").lastElementChild;
-			var avi = node.getElementsByClassName("avatar")[0];
-			var avimg = avi.getElementsByTagName("img")[0];
-			var mess = node.getElementsByClassName("messages")[0];
-			/*
-			mess.style.marginLeft = "40px";
-			avi.style.left = "0";
-
-			move(mess)
-				.duration('0.3s')
-				.ease('out')
-				.add('margin-left', 20)
-				.end();
-			
-			move(avi)
-				.duration('0s')
-				.ease('snap')
-				.scale('0.75')
-				.end();
-					
-			move(avi)
-				.duration('0.25s')
-				.ease('in')
-				.add('left', 10)
-				.scale('1')
-				.end();
-			*/
 			
 			move(node)
 				.set("-webkit-transform", "perspective( 500px ) rotateY(15deg)")
